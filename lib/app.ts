869fc7f3113ec9df75f1d55fd725b0e0b26fa97f
Christopher Blanchard <cablanchard@gmail.com>
2019-04-24 15:45:55 +0200

    First pass at implementing /parse

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,5 @@
 import e from "express";
-// import { Address } from "uk-clear-addressing";
+import { Address } from "uk-clear-addressing";
 
 const express = e;
 const OK = 200;
@@ -11,6 +11,49 @@ export const App = (): Express.Application => {
 
   app.get("/", (_, response) => {
     response.status(OK).send("<h1>Ping</h1>");
+  });
+
+  app.post("/parse", (request, response) => {
+    const {
+      building_name = "",
+      building_number = "",
+      sub_building_name = "",
+      dependant_locality = "",
+      double_dependant_locality = "",
+      thoroughfare = "",
+      dependant_thoroughfare = "",
+      po_box = "",
+      post_town = "",
+      postcode = "",
+      department_name = "",
+      organisation_name = "",
+    } = request.body;
+
+    const address = new Address(request.body);
+    const formatted = {
+      line_1: address.line_1,
+      line_2: address.line_2,
+      line_3: address.line_3,
+      post_town: address.post_town,
+      postcode: address.postcode,
+      premise: address.premise,
+    };
+
+    const query = {
+      building_name,
+      building_number,
+      sub_building_name,
+      dependant_locality,
+      double_dependant_locality,
+      thoroughfare,
+      dependant_thoroughfare,
+      po_box,
+      post_town,
+      postcode,
+      department_name,
+      organisation_name,
+    };
+    response.status(OK).json({ query, formatted });
   });
 
   app.all("/", (_, response) => {
--- test/app.ts
+++ test/app.ts
@@ -1,9 +1,30 @@
 import request from "supertest";
+import { postcodes } from "@ideal-postcodes/api-fixtures";
 import { App } from "../lib/app";
+import { assert } from "chai";
 
 const app = App();
 const OK = 200;
 const NOT_FOUND = 404;
+
+const testAddresses = postcodes.success.body.result;
+
+const testInput = testAddresses.map(address => {
+  return {
+    building_name: address.building_name,
+    building_number: address.building_number,
+    sub_building_name: address.sub_building_name,
+    dependant_locality: address.dependant_locality,
+    double_dependant_locality: address.double_dependant_locality,
+    thoroughfare: address.thoroughfare,
+    dependant_thoroughfare: address.dependant_thoroughfare,
+    po_box: address.po_box,
+    post_town: address.post_town,
+    postcode: address.postcode,
+    department_name: address.department_name,
+    organisation_name: address.organisation_name,
+  };
+});
 
 describe("Application server", () => {
   it("returns 200 OK on /", async () => {
@@ -16,5 +37,32 @@ describe("Application server", () => {
     const response = await request(app)
       .get("/foo")
       .expect(NOT_FOUND);
+  });
+
+  describe("POST /parse", () => {
+    let index = 0;
+    for (const address of testInput) {
+      it(`returns a correctly parsed address for "${
+        testAddresses[index].line_1
+      }, ${testAddresses[index].postcode}"`, async () => {
+        const response = await request(app)
+          .post("/parse")
+          .send(address)
+          .expect(OK);
+
+        assert.deepEqual(response.body, {
+          query: address,
+          formatted: {
+            line_1: testAddresses[index].line_1,
+            line_2: testAddresses[index].line_2,
+            line_3: testAddresses[index].line_3,
+            post_town: testAddresses[index].post_town,
+            postcode: testAddresses[index].postcode,
+            premise: testAddresses[index].premise,
+          },
+        });
+        index += 1;
+      });
+    }
   });
 });
